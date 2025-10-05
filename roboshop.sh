1949b6feb3b7cb871154bbff89330da4caf47d93
harikrishna-nagamalla <harikrishna884@gmail.com>
2025-10-06 05:06:53 +0530

    First roboshop script

--- roboshop.sh
+++ roboshop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 AMI_ID=ami-09c813fb71547fc4f
-SG_ID=sg-0424a85f21e5157ca
+SG_ID=sg-0424a85f21e5157ca # replace with your SG ID
 
-for instance in $@
+for instance in $@ # mongodb redis mysql
 
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f  --instance-type t3.micro --security-group-ids sg-0424a85f21e5157ca  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance == "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
