d84bf896b4f34bffb724f362b48cc8b8bd3c67d8
harikrishna-nagamalla <harikrishna884@gmail.com>
2025-10-06 05:31:18 +0530

    First roboshop script

--- roboshop.sh
+++ roboshop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-AMI_ID=ami-09c813fb71547fc4f
-SG_ID=sg-0424a85f21e5157ca # replace with your SG ID
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0424a85f21e5157ca" # replace with your SG ID
 
 for instance in $@ # mongodb redis mysql
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0424a85f21e5157ca --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     # Get Private IP
     if [ $instance != "frontend" ]; then
